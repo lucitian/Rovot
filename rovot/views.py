c1751cb5031e39959bfd57eb7d3fa666cf0071cd
student-iKairos <johnchris.torres@tup.edu.ph>
2021-04-11 20:14:48 +0800

    Registration Initial Commit

--- rovot/views.py
+++ rovot/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def login(request):
     return render(request, 'directories/login.html')
 
 def register(request):
-    return render(request, 'directories/register.html')
+    ctx = {
+            'form': None
+    }
+
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            uname = form.cleaned_data.get('username')
+
+    else:
+        form = UserRegistrationForm()
+
+    ctx['form'] = form
+
+    return render(request, 'directories/register.html', ctx)
 
 def home(request):
     return render(request, 'directories/home.html')
