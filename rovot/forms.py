2c2d43ee9f5dcd08f35cee2f8764fba91155019c
lucitian <flippery.slippery28@gmail.com>
2021-04-11 22:44:51 +0800

    Update UI for register screen

--- rovot/forms.py
+++ rovot/forms.py
@@ -3,7 +3,26 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField()
+    
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'register-input-username',
+        'type': 'text',
+    }), label='Username')
+
+    email = forms.EmailField(widget=forms.TextInput(attrs={
+        'class': 'register-input-email',
+        'type': 'text',
+    }), label='Email')
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'register-input-password',
+        'type': 'password',
+    }), label='Password')
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'register-input-confirm',
+        'type': 'password',
+    }), label='Confirm Password')
 
     class Meta:
         model = User 
